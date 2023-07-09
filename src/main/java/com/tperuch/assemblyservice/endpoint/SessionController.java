fe1cae620a5bcc11529b7e21ed884154b2352902
Thomas Peruch <peruchthomas@gmail.com>
2023-07-09 17:14:51 -0300

    add spring doc open api dependency and integrated with swagger ui

--- src/main/java/com/tperuch/assemblyservice/endpoint/SessionController.java
+++ src/main/java/com/tperuch/assemblyservice/endpoint/SessionController.java
@@ -3,6 +3,9 @@ package com.tperuch.assemblyservice.endpoint;
 import com.tperuch.assemblyservice.dto.SessionDto;
 import com.tperuch.assemblyservice.dto.response.SessionResponseDto;
 import com.tperuch.assemblyservice.service.SessionService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,16 +17,19 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/session")
+@Tag(name = "Sessões de votação", description = "Operações relacionadas as sessões de votação")
 public class SessionController {
 
     @Autowired
     private SessionService sessionService;
 
+    @Operation(summary = "Cria uma sessão de votação")
     @PostMapping
     public ResponseEntity<SessionResponseDto> createVotingSession(@Valid @RequestBody SessionDto sessionDto) {
         return new ResponseEntity(sessionService.openSession(sessionDto), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Busca todas as sessões")
     @GetMapping
     public ResponseEntity<List<SessionResponseDto>> findAllSession() {
         return new ResponseEntity(sessionService.findAllSessions(), HttpStatus.OK);
--- src/main/java/com/tperuch/assemblyservice/endpoint/TopicController.java
+++ src/main/java/com/tperuch/assemblyservice/endpoint/TopicController.java
@@ -3,6 +3,8 @@ package com.tperuch.assemblyservice.endpoint;
 import com.tperuch.assemblyservice.dto.TopicDto;
 import com.tperuch.assemblyservice.dto.response.TopicResponseDto;
 import com.tperuch.assemblyservice.service.TopicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +14,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/topic")
+@Tag(name = "Pautas", description = "Operações relacionadas somente a pautas")
 public class TopicController {
     @Autowired
     private TopicService service;
 
+    @Operation(summary = "Cria uma pauta")
     @PostMapping
     public ResponseEntity<TopicResponseDto> createTopic(@RequestBody TopicDto topicDto) {
         return new ResponseEntity<>(service.createTopic(topicDto), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Busca todos os pautas")
     @GetMapping
     public ResponseEntity<List<TopicResponseDto>> findAllTopics() {
         return new ResponseEntity<>(service.findAllTopics(), HttpStatus.OK);

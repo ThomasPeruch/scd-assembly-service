2b977a0f7baf78d0e9af4b269d96e696e3788021
Thomas Peruch <peruchthomas@gmail.com>
2023-07-09 01:12:41 -0300

    TopicService.createTopic() refactored and TopicFacade deleted

--- src/main/java/com/tperuch/assemblyservice/endpoint/TopicController.java
+++ src/main/java/com/tperuch/assemblyservice/endpoint/TopicController.java
@@ -2,7 +2,7 @@ package com.tperuch.assemblyservice.endpoint;
 
 import com.tperuch.assemblyservice.dto.TopicDto;
 import com.tperuch.assemblyservice.dto.response.TopicResponseDto;
-import com.tperuch.assemblyservice.facade.TopicFacade;
+import com.tperuch.assemblyservice.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/topic")
 public class TopicController {
     @Autowired
-    private TopicFacade topicFacade;
+    private TopicService service;
 
     @PostMapping
-    public ResponseEntity<TopicResponseDto> createTopic(@RequestBody TopicDto topicDto){
-        return new ResponseEntity<>(topicFacade.createTopic(topicDto), HttpStatus.CREATED);
+    public ResponseEntity<TopicResponseDto> createTopic(@RequestBody TopicDto topicDto) {
+        return new ResponseEntity<>(service.createTopic(topicDto), HttpStatus.CREATED);
     }
 }
--- src/main/java/com/tperuch/assemblyservice/service/TopicService.java
+++ src/main/java/com/tperuch/assemblyservice/service/TopicService.java
@@ -1,7 +1,11 @@
 package com.tperuch.assemblyservice.service;
 
+import com.tperuch.assemblyservice.dto.TopicDto;
+import com.tperuch.assemblyservice.dto.response.TopicResponseDto;
 import com.tperuch.assemblyservice.entity.TopicEntity;
 import com.tperuch.assemblyservice.repository.TopicRepository;
+import com.tperuch.assemblyservice.util.TopicUtil;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +15,22 @@ public class TopicService {
     @Autowired
     private TopicRepository topicRepository;
 
-    public TopicEntity createTopic(TopicEntity topicEntity){
-        return topicRepository.save(topicEntity);
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public TopicResponseDto createTopic(TopicDto topicDto){
+        TopicEntity topicEntity = mapToEntity(topicDto);
+        TopicEntity savedEntity = topicRepository.save(topicEntity);
+        return modelMapper.map(savedEntity, TopicResponseDto.class);
     }
 
     public boolean existsTopic(Long id){
         return topicRepository.existsById(id);
     }
 
+    private TopicEntity mapToEntity(TopicDto topicDto){
+        TopicEntity topicEntity = new TopicEntity();
+        topicEntity.setName(topicDto.getName());
+        return topicEntity;
+    }
 }
